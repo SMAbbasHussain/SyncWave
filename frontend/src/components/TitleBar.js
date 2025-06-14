77c651f5a572ab377a2eaffd0ac0f5c805d9faf5
SMAbbasHussain <abbashussain4359@gmail.com>
2025-06-14 07:35:22 +0500

    Desktop Version ka panga

---
+++ frontend/src/components/TitleBar.js
@@ -0,0 +1,19 @@
+// src/components/TitleBar.js
+import React from 'react';
+import './TitleBar.css'; // We'll create this file next
+
+const TitleBar = () => {
+  return (
+    <div className="title-bar">
+      <div className="title">
+        <img src="/logo192.png" alt="logo" className="icon" /> {/* Example icon */}
+        SyncWave
+      </div>
+      <div className="window-controls">
+         {/* We will add buttons here later */}
+      </div>
+    </div>
+  );
+};
+
+export default TitleBar;
